158abfd3c44f838810f9071bc4114a4e29b9135b
Neil Daftary <neil98daftary@gmail.com>
2017-08-11 15:01:13 +0530

    Initial structure set up for frontend

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
+from flask
 
 @app.route('/')
+def initialise():
 @app.route('/index')
 def index():
     return "Hello, World!"
--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 #!flask/bin/python
-from app import app
-app.run(debug=True)
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route('/')
+def initialise():
+	return render_template('index.html')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+    
+if __name__=='__main__':
+	app.run(debug=True)
