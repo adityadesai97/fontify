82c0724cfc83ca13b0d3e781b26321a0040f4e5c
Aditya Desai <adityadesai97@gmail.com>
2017-09-22 10:02:10 +0530

    Major changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+engine = create_engine('sqlite:///tutorial.db', echo=True)
 
 app = Flask(__name__)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,8 +1,99 @@
+import cv2
+import os
+
 from app import app
-from flask
+from flask import render_template, redirect, flash, request, send_from_directory, session, abort
+from werkzeug.utils import secure_filename
+
+from fontify import image_process as imp
 
 @app.route('/')
-def initialise():
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/image', methods=['GET', 'POST'])
+def submit_image():
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file:
+            filename = secure_filename(file.filename)
+            dirpath = os.path.join(app.root_path, 'users')
+            filepath = os.path.join(dirpath, filename)
+            file.save(filepath)
+
+            img = cv2.imread(filepath)
+
+            imp.split(img, dirpath)
+            imp.font_generate('user.ttf', dirpath)
+
+            return render_template('download_page.html')
+
+    return render_template('upload_page.html')
+
+@app.route('/step1')
+def index():
+    return render_template('step1.html')
+
+@app.route('/step2')
+def index1():
+    return render_template('step2.html')
+
+@app.route('/edit')
+def index2():
+    return render_template('editor.html')
+
+@app.route('/login')
+def index3():
+    return render_template('login.html')
+
+@app.route('/signup')
+def index4():
+    return render_template('signup.html')
+
+@app.route('/check', methods=['POST'])
+def do_admin_login():
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return initialise()
+
+@app.route('/check1', methods=['POST'])
+def do_admin_signup():
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    user = User(POST_USERNAME,POST_PASSWORD)
+    session.add(user)
+    session.commit()
+    return do_admin_login()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return initialise()
+
+@app.route('/font', methods=['GET'])
+def get_font():
+    dirpath = os.path.join(app.root_path, 'users')
+    filename = 'user.ttf'
+
+    return send_from_directory(directory=dirpath, filename=filename)
